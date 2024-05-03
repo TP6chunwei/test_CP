d2db96069dfee952c929ed3020b516bf8b1adcae
TP6chunwei <153341491+TP6chunwei@users.noreply.github.com>
2024-05-03 19:29:46 +0800

    app.py

--- app.py
+++ app.py
@@ -1,42 +1,58 @@
 from flask import Flask, request, abort
-
+import requests
+import statistics
+import os
+import requests
+# from bs4 import BeautifulSoup
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable 
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import openai
-import time
-import traceback
-#======python的函數庫==========
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
+    PostbackEvent, MemberJoinedEvent, LocationMessage
+)
 
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-# OPENAI API Key初始化設定
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
+# Channel Access Token and Secret (Note: These should ideally be stored securely, not hardcoded)
+access_token = "QFVheezi84Iy1z9+jhHfMAQLG7W2qtfs2BJVn18HVxt96WxxLzdGXzbdycnGiXkUk7g3wn7LIdmXbzuo7+s2mUX4I99hf2xSCq4ysfAHK/c8kpug7Vq6k458Js+An0XVhvNENn9Km2OuHL5cFhU9YQdB04t89/1O/w1cDnyilFU="
+channel_secret = "e752f446b5fd2237eaa8d61ae077ec93"
+line_bot_api = LineBotApi(access_token)
+handler = WebhookHandler(channel_secret)
+
+
+def reply_weather_image(reply_token):
+    try:
+        radar_url = 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0058-003?Authorization=rdec-key-123-45678-011121314&format=JSON'
+        radar = requests.get(radar_url)
+        radar_json = radar.json()
+        radar_img = radar_json['cwaopendata']['dataset']['resource']['ProductURL']
+        radar_time = radar_json['cwaopendata']['dataset']['DateTime']
+
+        line_bot_api.reply_message(
+            reply_token,
+            ImageSendMessage(
+                original_content_url=radar_img,
+                preview_image_url=radar_img
+            )
+        )
+    except Exception as e:
+        print(f"Error replying with weather image: {e}")
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    try:
-        GPT_answer = GPT_response(msg)
-        print(GPT_answer)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
-    except:
-        print(traceback.format_exc())
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
+    if  event.message.type == 'text':
+        msg = event.message.text
+        if msg.lower() in ['雷達回波圖']:
+            reply_weather_image(event.reply_token)
        
         
 import os
